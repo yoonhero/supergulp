0751cd71b80dc71b722f82184a5f6ca84d4a53c7
yoonhero <yoonhero06@naver.com>
2021-06-04 07:21:07 +0900

    6/4 #5

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,8 @@ import image from "gulp-image"
 import sass from "gulp-sass";
 import autofixer from "gulp-autoprefixer"
 import miniCss from "gulp-csso"
+import bro from "gulp-bro"
+import babelify from "babelify"
 
 sass.compiler = require("node-sass");
 
@@ -23,6 +25,11 @@ const routes = {
     watch: "src/scss/**/*.scss",
     src: "src/scss/style.scss",
     dest: "build/css"
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js"
   }
 };
 const pug = () =>
@@ -51,17 +58,25 @@ const styles = () =>
     .pipe(miniCss())
     .pipe(gulp.dest(routes.scss.dest));
 
+const js = () => gulp.src(routes.js.src).pipe(bro({
+  transform: [
+    babelify.configure({ presets: ["@babel/preset-env"] }),
+    ["uglifyify", { global: true }]
+  ]
+})).pipe(gulp.dest(routes.js.dest))
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug)
   gulp.watch(routes.img.src, img)
   gulp.watch(routes.scss.watch, styles)
+  gulp.watch(routes.js.watch, js)
 }
 
 
 const prepare = gulp.series([clean, img])
 
 
-const assets = gulp.series([pug, styles])
+const assets = gulp.series([pug, styles, js])
 
 const postDev = gulp.parallel([webserver, watch]);
 
