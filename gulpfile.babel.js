ce5bd247b5bc0f9a792c8a854a916d6cc8b77968
yoonhero <yoonhero06@naver.com>
2021-06-04 07:08:31 +0900

    6/4 #4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,11 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image"
+import sass from "gulp-sass";
+import autofixer from "gulp-autoprefixer"
+import miniCss from "gulp-csso"
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
@@ -13,6 +18,11 @@ const routes = {
   img: {
     src: "src/img/*",
     dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css"
   }
 };
 const pug = () =>
@@ -21,23 +31,37 @@ const pug = () =>
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
+
+
 const clean = () => del(["build/"])
 
-const webserver = () => gulp.src("build").pipe(ws({ livelad: true, open: true }))
+const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
+
+
 
 const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest))
 
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autofixer({
+      browsers: ["last 2 versions"]
+    }))
+    .pipe(miniCss())
+    .pipe(gulp.dest(routes.scss.dest));
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug)
   gulp.watch(routes.img.src, img)
+  gulp.watch(routes.scss.watch, styles)
 }
 
 
 const prepare = gulp.series([clean, img])
 
 
-const assets = gulp.series([pug])
+const assets = gulp.series([pug, styles])
 
 const postDev = gulp.parallel([webserver, watch]);
 
